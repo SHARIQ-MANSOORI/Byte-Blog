6d88c2ba05b3680bc41e7bb49f4010dc8f9767b7
sharik <csh23030@glbitm.ac.in>
2026-03-04 22:51:14 +0530

    Set up ENVs

---
+++ src/configs/Configs.js
@@ -0,0 +1,13 @@
+const configs = {
+    appwriteUrl:String(import.meta.VITE_APPWRITE_URL),
+
+    appwriteProjectId:String(import.meta.VITE_APPWRITE_PROJECT_ID),
+
+    appwriteDataBaseId : String(import.meta.VITE_APPWRITE_DATABASE_ID),
+
+    appwriteCollectionId : String(import.meta.VITE_APPWRITE_COLLECTION_ID),
+
+    appwriteBucketId : String(import.meta.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default configs;
