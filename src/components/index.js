b41a1c57d7345d520403e83cd55289991aa6198a
sharik <csh23030@glbitm.ac.in>
2026-03-14 11:11:27 +0530

    Create header , footer , input and button comp.

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,12 @@
 import Header from "./Header/Header.jsx";
 import Footer  from "./Footer/Footer.jsx";
-
+import Conatainer from './Container/Container.jsx'
+import Logo from './Logo.jsx'
+import LogoutBtn from "./Header/LogoutBtn.jsx";
 export {
     Header,
-    Footer
+    Footer,
+    Conatainer,
+    Logo,
+    LogoutBtn
 }
