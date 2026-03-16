454e9aad37e29a49368c465ba9e7a1ba37861271
sharik <csh23030@glbitm.ac.in>
2026-03-16 12:12:18 +0530

    Refactor components: remove Button, update Login layout, add Signup component with form handling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -30,9 +30,9 @@ function Login() {
   return (
     <div className='flex items-center justify-center w-full'>
         <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
-        <div className='mb-2 inline-block w-full max-w-[100px]'>
-            <span>
-                <Logo></Logo>
+        <div className='mb-2 flex justify-center'>
+            <span className='inline-block w-full max-w-[100px]'>
+                <Logo width='100%'/>
             </span>
         </div>
         <h2 className='text-center text-2xl font-bold
--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,14 @@ import Footer  from "./Footer/Footer.jsx";
 import Conatainer from './Container/Container.jsx'
 import Logo from './Logo.jsx'
 import LogoutBtn from "./Header/LogoutBtn.jsx";
+import Button from './Button.jsx'
+import Input from './Input.jsx'
 export {
     Header,
     Footer,
     Conatainer,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input
 }
